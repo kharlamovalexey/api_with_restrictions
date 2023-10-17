2ca44c1539517a4f320341a678d0c15e3aa7ece7
kharlamovalexey <128462782+kharlamovalexey@users.noreply.github.com>
2023-10-17 20:37:04 +0300

    Update settings.py

--- api_with_restrictions/settings.py
+++ api_with_restrictions/settings.py
@@ -97,19 +97,12 @@ WSGI_APPLICATION = 'api_with_restrictions.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'netology_classified_ads',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
     }
 }
-
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.postgresql',
-#         'NAME': 'netology_classified_ads',
-#         'HOST': '127.0.0.1',
-#         'PORT': '5432',
-#     }
-# }
 
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
